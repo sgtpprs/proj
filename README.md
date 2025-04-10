00ac64548bb3acd6c571cc97b58283cd59dd842a
sgtpprs <armandonieto2003@gmail.com>
2025-04-10 02:50:08 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,87 @@
+# US-Gas-Prices
+
+Python scripts that scrape US gas prices.
+
+> 👋 This repository's maintainer is available to hire for US gas prices data-extraction consulting projects. To get a cost estimate, send email to lallyelias87@gmail.com (for projects of any size or complexity).
+
+## Requirements
+
+- [Python 3.8.17+](https://www.python.org/)
+- [pip 24.2+](https://github.com/pypa/pip)
+- [selenium 4.14.0+](https://github.com/SeleniumHQ/selenium/tree/trunk/py)
+- [webdriver-manager 4.0.2+](https://github.com/SergeyPirogov/webdriver_manager)
+- [pandas 2.0.3+](https://github.com/pandas-dev/pandas)
+
+## Usage
+
+- Clone this repository
+```sh
+git clone https://github.com/lykmapipo/US-Gas-Prices.git
+cd US-Gas-Prices
+```
+
+- Install all dependencies
+
+```sh
+pip install -r requirements.txt
+```
+
+- To scrape [state gas price daily averages](https://gasprices.aaa.com/state-gas-price-averages/), run:
+
+```sh
+python scrape_state_daily_averages.py
+```
+
+- To scrape [metro gas price daily averages](https://gasprices.aaa.com/state-gas-price-averages/), run:
+
+```sh
+python scrape_metro_daily_averages.py
+```
+
+## Data Exploration
+The scraped data are saved in `csv` format, and may be found under `data` folder.
+
+- Explore part of the data using `pandas`, use:
+```python
+import pandas as pd
+
+# file = "./data/metro-daily-averages/2023-12-27.csv"
+file = "./data/state-daily-averages/2023-12-27.csv"
+df = pd.read_csv(file)
+
+df.info()
+```
+
+- To explore all the data using `pandas`, use:
+```python
+import glob
+import pandas as pd
+
+# files = glob.glob("./data/metro-daily-averages/*.csv")
+files = glob.glob("./data/state-daily-averages/*.csv")
+dfs = [pd.read_csv(file) for file in files]
+df = pd.concat(dfs, ignore_index=True)
+
+df.info()
+```
+
+## Contribute
+
+It will be nice, if you open an issue first so that we can know what is going on, then, fork this repo and push in your ideas. Do not forget to add a bit of test(s) of what value you adding.
+
+## Questions/Contacts
+
+lallyelias87@gmail.com, or open a GitHub issue
+
+
+## Licence
+
+The MIT License (MIT)
+
+Copyright (c) lykmapipo & Contributors
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ scrape_state_daily_averages.py
@@ -0,0 +1,140 @@
+"""
+Scrape state gas price daily averages from AAA Gas Prices website.
+
+Usage::
+
+    mkdir -p data/state-daily-averages
+    pip install selenium webdriver-manager pandas
+    python scrape_state_daily_averages.py
+
+"""
+
+import datetime
+import logging
+import os
+import random
+from pathlib import Path
+
+import pandas as pd
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from webdriver_manager.chrome import ChromeDriverManager
+
+SOURCE_DEFAULT_URL = "https://gasprices.aaa.com/state-gas-price-averages/"
+SOURCE_URL = os.environ.get("SOURCE_URL") or SOURCE_DEFAULT_URL
+
+DATE_CSS_SELECTOR = "div.average-price > span"
+HEADERS_CSS_SELECTOR = "#sortable > thead > tr > th"
+ROWS_CSS_SELECTOR = "#sortable > tbody > tr"
+
+DATASETS_DEFAULT_BASE_PATH = Path("./data")
+DATASETS_BASE_PATH = Path(os.environ.get("DATASETS_BASE_PATH") or DATASETS_DEFAULT_BASE_PATH)
+
+HEADERS_BASIC = ["State-Name", "State-Abbreviation"]
+HEADERS_CURRENCY = "Currency"
+HEADERS_UNIT = "Unit"
+HEADERS_DATE = "Date"
+CURRENCY = "U.S Dollar"
+UNIT = "US Gallon"
+
+DRIVER_WAIT_TIME_MAXIMUM = 10
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
+
+# prepare chrome
+logging.info("Start.")
+logging.info("Preparing chrome ...")
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--incognito")
+chrome_options.add_argument("--no-default-browser-check")
+chrome_options.add_argument("--disable-first-run-ui")
+chrome_options.add_argument("--no-first-run")
+chrome_options.add_argument("--disable-blink-features=AutomationControlled")
+chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
+chrome_options.add_experimental_option("useAutomationExtension", False)
+chrome_options.add_argument("--disable-sync")
+
+window_width = random.randint(900, 1920)
+window_height = random.randint(768, 1080)
+logging.info(f"Set window-size to {window_width},{window_height}")
+chrome_options.add_argument(f"--window-size={window_width},{window_height}")
+
+chrome_driver_manager = ChromeDriverManager()
+chrome_executable_path = chrome_driver_manager.install()
+chrome_service = webdriver.ChromeService(executable_path=chrome_executable_path)
+
+chrome_driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
+chrome_driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
+logging.info("Preparing chrome finished.")
+
+# request page
+logging.info("Requesting page ...")
+chrome_driver.get(SOURCE_URL)
+
+# wait for document to be ready to use JavaScript
+logging.info("Waiting for document to be ready to use JavaScript ...")
+while chrome_driver.execute_script("return document.readyState") != "complete":
+    logging.info("Waiting for document ready state ...")
+
+# wait for common elements to be present on the page (i.e map, tables ...)
+logging.info("Waiting for document common elements to be present ...")
+chrome_driver_wait = WebDriverWait(chrome_driver, DRIVER_WAIT_TIME_MAXIMUM)
+expected_conditions = (
+    EC.presence_of_element_located((By.CSS_SELECTOR, DATE_CSS_SELECTOR)),  # price date
+    EC.presence_of_all_elements_located((By.CSS_SELECTOR, HEADERS_CSS_SELECTOR)),  # table header
+    EC.presence_of_all_elements_located((By.CSS_SELECTOR, ROWS_CSS_SELECTOR)),  # table rows
+)
+elements = chrome_driver_wait.until(EC.all_of(*expected_conditions))
+logging.info("Requesting page finished.")
+
+# parse gas price date
+logging.info("Parsing data ...")
+try:
+    scrape_date = chrome_driver.find_element(By.CSS_SELECTOR, DATE_CSS_SELECTOR)
+    scrape_date = scrape_date.text.strip().split()[-1].strip()
+    scrape_date = datetime.datetime.strptime(scrape_date, "%m/%d/%y").date()
+except:
+    scrape_date = datetime.date.today()
+
+# parse data headers
+headers = chrome_driver.find_elements(By.CSS_SELECTOR, HEADERS_CSS_SELECTOR)
+headers = HEADERS_BASIC + [header.text.strip() for header in headers[1:]]
+
+# parse data rows
+rows = chrome_driver.find_elements(By.CSS_SELECTOR, ROWS_CSS_SELECTOR)
+
+
+def parse_row(row):
+    tds = row.find_elements(By.TAG_NAME, "td")
+    state_td = tds[0].find_element(By.TAG_NAME, "a")
+    state_name = state_td.text.strip()
+    state_abbreviation = state_td.get_attribute("href").split("=")[-1].strip().upper()
+    prices = [float(price_td.text.replace("$", "").strip()) for price_td in tds[1:]]
+    return [state_name, state_abbreviation] + prices
+
+
+rows = map(parse_row, rows)
+
+# collect data
+data = [dict(zip(headers, row)) for row in rows]
+df = pd.DataFrame(data)
+df[HEADERS_CURRENCY] = CURRENCY
+df[HEADERS_UNIT] = UNIT
+df[HEADERS_DATE] = scrape_date
+logging.info("Parsing data finished.")
+
+# save data
+data_pth = DATASETS_BASE_PATH / "state-daily-averages" / f"{scrape_date}.csv"
+logging.info(f"Saving data at {data_pth}")
+data_pth = data_pth.expanduser().resolve()
+data_pth.parent.mkdir(parents=True, exist_ok=True)
+df.to_csv(data_pth, index=False)
+logging.info("Saving data finished.")
+
+# close chrome
+chrome_driver.close()
+chrome_driver.quit()
+logging.info("Done.")
